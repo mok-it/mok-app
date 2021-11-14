9ffeb9627d183e29d7f286d651f3f3f9db83e69a
Simeon Kuzman <nemeth.agoston@edu.bme.hu>
2021-11-14 13:02:15 +0100

    Egyéb

--- MOKApp/app/src/main/java/mok/it/app/mokapp/projects/ListActivity.kt
+++ MOKApp/app/src/main/java/mok/it/app/mokapp/projects/ListActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.bumptech.glide.Glide
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.activity_list.*
 import kotlinx.android.synthetic.main.activity_main.textView2
 import mok.it.app.mokapp.R
@@ -13,23 +14,26 @@ import mok.it.app.mokapp.auth.LoginActivity
 class ListActivity : AppCompatActivity() {
 
     private lateinit var mAuth: FirebaseAuth
+    private lateinit var currentUser: FirebaseUser
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list)
 
         mAuth = FirebaseAuth.getInstance()
-        val currentUser = mAuth.currentUser
+        if(mAuth.currentUser == null) {
+            // TODO handle null user
+        } else {
+            currentUser = mAuth.currentUser!!
+        }
 
-        textView2.text = currentUser?.displayName
-        textView3.text = currentUser?.email
+        //Glide.with(this).load(currentUser?.photoUrl).into(imageView)
 
-        Glide.with(this).load(currentUser?.photoUrl).into(imageView)
-
-        button.setOnClickListener{
+        /*button.setOnClickListener{
             mAuth.signOut()
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
             finish()
-        }
+        }*/
     }
 }

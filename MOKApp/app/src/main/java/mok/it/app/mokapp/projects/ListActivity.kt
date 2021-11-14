6455ab26e0c67adeada28e70047e73ab4d7162e3
Simeon Kuzman <nemeth.agoston@edu.bme.hu>
2021-11-14 13:17:31 +0100

    List view binding, login handling

--- MOKApp/app/src/main/java/mok/it/app/mokapp/projects/ListActivity.kt
+++ MOKApp/app/src/main/java/mok/it/app/mokapp/projects/ListActivity.kt
@@ -6,49 +6,49 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.TextView
-import com.bumptech.glide.Glide
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter
 import com.firebase.ui.firestore.FirestoreRecyclerOptions
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_list.*
-import kotlinx.android.synthetic.main.activity_main.textView2
 import mok.it.app.mokapp.R
 import mok.it.app.mokapp.auth.LoginActivity
 
 class ListActivity : AppCompatActivity() {
 
     private lateinit var mAuth: FirebaseAuth
+    private lateinit var currentUser: FirebaseUser
+
+    private lateinit var recyclerView: RecyclerView
+
     val firestore = Firebase.firestore;
-    val projectCollectionPath: String = "test";
+    val projectCollectionPath: String = "/projects";
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list)
 
         mAuth = FirebaseAuth.getInstance()
         if(mAuth.currentUser == null) {
-            // TODO handle null user
+            // TODO handle null user - is this enough?
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
         } else {
             currentUser = mAuth.currentUser!!
         }
 
-        //Glide.with(this).load(currentUser?.photoUrl).into(imageView)
-
-        /*button.setOnClickListener{
-            mAuth.signOut()
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
-            finish()
-        }
-
-        val query = firestore.collection(projectCollectionPath);
+        val query = firestore.collection(projectCollectionPath)
         val options = FirestoreRecyclerOptions.Builder<ProjectListElement>().setQuery(query, ProjectListElement::class.java)
-            .setLifecycleOwner(this).build();
+            .setLifecycleOwner(this).build()
         val adapter = object: FirestoreRecyclerAdapter<ProjectListElement, ProjectViewHolder>(options){
             override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProjectViewHolder {
-                val view = LayoutInflater.from(this@ListActivity).inflate(android.R.layout.simple_list_item_2, parent, false);
-                return ProjectViewHolder(view);
+                val view = LayoutInflater.from(this@ListActivity).inflate(R.layout.project_card, parent, false)
+                return ProjectViewHolder(view)
             }
 
             override fun onBindViewHolder(
@@ -56,17 +56,21 @@ class ListActivity : AppCompatActivity() {
                 position: Int,
                 model: ProjectListElement
             ) {
-                // TODO assign the proper ID-s to the variables
-                val tvName: TextView = holder.itemView.findViewById(android.R.id.text1)
-                val tvDesc: TextView = holder.itemView.findViewById(android.R.id.text1)
-                tvName.text = model.name;
-                tvDesc.text = model.description;
+                val tvName: TextView = holder.itemView.findViewById(R.id.projectName)
+                val tvDesc: TextView = holder.itemView.findViewById(R.id.projectDescription)
+                tvName.text = model.name
+                tvDesc.text = model.description
                 //TODO project icon
                 //val imgIcon: Image = ...
+                // ?? Glide.with(this).load(currentUser?.photoUrl).into(imageView)
             }
         }
-        //TODO ez mi?
-       /* rvUsers.adapter = adapter
-        rvUsers.layoutManager = LinearLayoutManager(this)*/
+
+        recyclerView = findViewById(R.id.recyclerView)
+
+        // Bind data to the list
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
     }
 }

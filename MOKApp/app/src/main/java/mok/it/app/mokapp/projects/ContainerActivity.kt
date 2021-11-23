a247c2ac7e557c1eab9e18a3e876fd43c19993d2
Zalan <gyarzal@gmail.com>
2021-11-23 21:55:51 +0100

    Drawer Menu
    Layout struktúra kialakítása

---
+++ MOKApp/app/src/main/java/mok/it/app/mokapp/projects/ContainerActivity.kt
@@ -0,0 +1,12 @@
+package mok.it.app.mokapp.projects
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import mok.it.app.mokapp.R
+
+class ContainerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_container)
+    }
+}

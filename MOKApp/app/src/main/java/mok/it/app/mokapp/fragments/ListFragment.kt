4c0a09ceb69d727dccb84682bed498e94edf92c8
Simeon Kuzman <nemeth.agoston@edu.bme.hu>
2022-01-17 19:49:53 +0100

    Badge creation layout init, minor list design changes

--- MOKApp/app/src/main/java/mok/it/app/mokapp/fragments/ListFragment.kt
+++ MOKApp/app/src/main/java/mok/it/app/mokapp/fragments/ListFragment.kt
@@ -15,6 +15,7 @@ import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_list.*
 import mok.it.app.mokapp.R
 import android.util.Log
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.squareup.picasso.Picasso
 import mok.it.app.mokapp.model.ProjectListElement
 import mok.it.app.mokapp.recyclerview.ProjectViewHolder
@@ -70,6 +71,14 @@ class ListFragment : Fragment() {
         recyclerView.layoutManager =
                 WrapContentLinearLayoutManager(this.context)
         //recyclerView.layoutManager = LinearLayoutManager(this.context)
+
+        view.findViewById<FloatingActionButton>(R.id.addBadgeButton).also { actionButton ->
+            actionButton.setOnClickListener {
+
+                // TODO handle by opening create badge fragment
+
+            }
+        }
 
     }
 

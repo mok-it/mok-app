a160687d69cecf57718c276db2867db388793475
Simeon Kuzman <nemeth.agoston@edu.bme.hu>
2022-02-14 14:19:14 +0100

    Badge creation fragment

---
+++ MOKApp/app/src/main/java/mok/it/app/mokapp/activity/CreateBadgeFragment.kt
@@ -0,0 +1,73 @@
+package mok.it.app.mokapp.activity
+
+import android.app.AlertDialog
+import android.graphics.Rect
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.DialogFragment
+import mok.it.app.mokapp.R
+
+
+/**
+ * TODO description
+ */
+
+class CreateBadgeFragment : DialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_create_badge, container, false).also { dialogView ->
+
+            val builder = AlertDialog.Builder(context)
+
+            val displayRectangle = Rect(100, 100, 100, 100)
+
+            dialogView.minimumWidth = (displayRectangle.width() * 1f).toInt()
+            dialogView.minimumHeight = (displayRectangle.height() * 1f).toInt()
+            builder.setView(dialogView)
+            val alertDialog = builder.create()
+            val buttonOk: Button = dialogView.findViewById(R.id.button2)
+            buttonOk.setOnClickListener(View.OnClickListener { alertDialog.dismiss() })
+            alertDialog.show()
+        }
+    }
+
+    companion object {
+
+        val TAG = "CreateBadgeFragment"
+
+        fun newInstance() =
+            CreateBadgeFragment()
+
+        /*fun createDialog(view: View, fragment: Fragment) {
+            val displayRectangle = Rect()
+            fragment.activity?.window?.let { window ->
+                window.getDecorView().getWindowVisibleDisplayFrame(displayRectangle)
+                val builder = AlertDialog.Builder(fragment.context)
+                val viewGroup: ViewGroup = fragment.view.findViewById()
+                val dialogView: View =
+                    LayoutInflater.from(view.getContext())
+                        .inflate(R.layout.fragment_create_badge, viewGroup, false)
+                dialogView.minimumWidth = (displayRectangle.width() * 1f) as Int
+                dialogView.minimumHeight = (displayRectangle.height() * 1f) as Int
+                builder.setView(dialogView)
+                val alertDialog = builder.create()
+                val buttonOk: Button = dialogView.findViewById(R.id.button2)
+                buttonOk.setOnClickListener(View.OnClickListener { alertDialog.dismiss() })
+                alertDialog.show()
+            } ?: run {
+
+                Log.e(TAG, "Could not create dialog")
+
+            }
+
+        }*/
+
+    }
+}

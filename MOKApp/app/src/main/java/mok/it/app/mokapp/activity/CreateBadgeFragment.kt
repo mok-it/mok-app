35ad225d1655703322dd878f135be04ccf8c1ad6
Simeon Kuzman <nemeth.agoston@edu.bme.hu>
2022-02-17 21:26:15 +0100

    Create badge improvements

--- MOKApp/app/src/main/java/mok/it/app/mokapp/activity/CreateBadgeFragment.kt
+++ MOKApp/app/src/main/java/mok/it/app/mokapp/activity/CreateBadgeFragment.kt
@@ -1,13 +1,21 @@
 package mok.it.app.mokapp.activity
 
 import android.app.AlertDialog
-import android.graphics.Rect
+import android.app.Dialog
+import android.content.DialogInterface
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageButton
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
+import androidx.cardview.widget.CardView
 import androidx.fragment.app.DialogFragment
+import com.google.android.material.textfield.TextInputEditText
 import mok.it.app.mokapp.R
 
 
@@ -16,6 +24,15 @@ import mok.it.app.mokapp.R
  */
 
 class CreateBadgeFragment : DialogFragment() {
+
+    lateinit var nameTIET: TextInputEditText
+    lateinit var descriptionTIET: TextInputEditText
+    lateinit var iconSelectCard: CardView
+    lateinit var closeButton: ImageButton
+    lateinit var createButton: Button
+
+    private val isNameRequired = true
+    private val isDescriptionRequired = true
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,52 +45,148 @@ class CreateBadgeFragment : DialogFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        /*val builder = AlertDialog.Builder(context)
+        view.apply {
 
-        val displayRectangle = Rect(0, 0, 200, 100)
+            nameTIET = findViewById(R.id.badge_name)
+            descriptionTIET = findViewById(R.id.badge_description)
+            iconSelectCard = findViewById(R.id.icon_select_card)
+            closeButton = findViewById(R.id.close_button)
+            createButton = findViewById(R.id.create_button)
 
-        view.minimumWidth = (displayRectangle.width() * 1f).toInt()
-        view.minimumHeight = (displayRectangle.height() * 1f).toInt()*/
+            closeButton.setOnClickListener {
+                onCloseButtonPressed()
+            }
 
+            createButton.setOnClickListener {
+                onCreateBadgePressed()
+            }
+        }
+    }
 
-        /*builder.setView(view)
-        val alertDialog = builder.create()
-        val buttonOk: Button = view.findViewById(R.id.button2)
-        buttonOk.setOnClickListener(View.OnClickListener { alertDialog.dismiss() })
-        alertDialog.show()*/
-        
+    override fun onStart() {
+        super.onStart()
+        val dialog: Dialog? = dialog
+
+        dialog?.also {
+            val width = ViewGroup.LayoutParams.MATCH_PARENT
+            val height = ViewGroup.LayoutParams.MATCH_PARENT
+            it.window?.setLayout(width, height)
+
+            isCancelable = false
+        }
+    }
+
+    /**
+     * Called if the dialog needs to be closed.
+     */
+    private fun closeDialog() {
+        Toast.makeText(context, "Close dialog", Toast.LENGTH_SHORT).show() // todo remove
+        dialog?.dismiss() ?: run {
+            Log.w(TAG, "Can not close dialog, it is null.")
+        }
+    }
+
+    /**
+     * Called if the user wants to close the dialog.
+     */
+    private fun onCloseButtonPressed() {
+
+        if (isBlank()) {
+            closeDialog()
+            return
+        }
+
+        /**
+         * The user has unsaved changes, thus warning them before closing the dialog
+         */
+        AlertDialog.Builder(context)
+            .setCancelable(false)
+            .setMessage(R.string.unsaved_changes)
+            .setPositiveButton(
+                R.string.discard
+            ) { _, _ -> closeDialog() }
+            .setNegativeButton(R.string.edit, null)
+            .create()
+            .show()
+
+    }
+
+    /**
+     *  Creates a new badge in the database.
+     *  @return
+     */
+    private fun commitNewBadgeToDatabase(): Boolean {
+        // TODO upload badge metadata to firebase
+    }
+
+    /**
+     * Called if the Create button is pressed in the dialog.
+     * @return whether the dialog should be closed
+     */
+    private fun onCreateBadge(): Boolean {
+        if (!isComplete(true)) {
+            return false
+        }
+
+        val success = commitNewBadgeToDatabase()
+
+        return success
+    }
+
+    /**
+     * Called if the user wants to create the badge.
+     */
+    private fun onCreateBadgePressed() {
+        val shouldCloseDialog = onCreateBadge()
+
+        if (shouldCloseDialog) {
+            closeDialog()
+        }
+    }
+
+    /**
+     * TODO Known bug: the contents of an input field seems to always register as blank
+     * if the cursor is in it
+     *
+     * @return false if there exists any user-given piece of information in the dialog
+     */
+    private fun isBlank() = when {
+
+        nameTIET.text.isNullOrBlank() -> true
+        descriptionTIET.text.isNullOrBlank() -> true
+
+        else -> false
+    }
+
+    /**
+     * @return if all required fields are complete. If something is missing, can warn the user.
+     */
+    private fun isComplete(showWarning: Boolean): Boolean = when {
+        isNameRequired && nameTIET.text.isNullOrBlank() -> {
+            if (showWarning) Toast.makeText(
+                context,
+                R.string.badge_name_is_required,
+                Toast.LENGTH_SHORT
+            ).show() // TODO snackbar
+            false
+        }
+
+        isDescriptionRequired && descriptionTIET.text.isNullOrBlank() -> {
+            if (showWarning) Toast.makeText(
+                context,
+                R.string.badge_description_is_required,
+                Toast.LENGTH_SHORT
+            ).show() // TODO snackbar
+            false
+        }
+
+        else -> true
     }
 
     companion object {
-
         val TAG = "CreateBadgeFragment"
 
-        fun newInstance() =
-            CreateBadgeFragment()
-
-        /*fun createDialog(view: View, fragment: Fragment) {
-            val displayRectangle = Rect()
-            fragment.activity?.window?.let { window ->
-                window.getDecorView().getWindowVisibleDisplayFrame(displayRectangle)
-                val builder = AlertDialog.Builder(fragment.context)
-                val viewGroup: ViewGroup = fragment.view.findViewById()
-                val dialogView: View =
-                    LayoutInflater.from(view.getContext())
-                        .inflate(R.layout.fragment_create_badge, viewGroup, false)
-                dialogView.minimumWidth = (displayRectangle.width() * 1f) as Int
-                dialogView.minimumHeight = (displayRectangle.height() * 1f) as Int
-                builder.setView(dialogView)
-                val alertDialog = builder.create()
-                val buttonOk: Button = dialogView.findViewById(R.id.button2)
-                buttonOk.setOnClickListener(View.OnClickListener { alertDialog.dismiss() })
-                alertDialog.show()
-            } ?: run {
-
-                Log.e(TAG, "Could not create dialog")
-
-            }
-
-        }*/
+        fun newInstance() = CreateBadgeFragment()
 
     }
 }

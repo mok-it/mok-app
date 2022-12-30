33125a6d734b505e110fac5be44bc9633971bc8a
Oliver Raisz <raiszoliver@gmail.com>
2022-12-30 20:21:13 +0100

    solved tablet compatibility issue

--- MOKApp/app/src/main/java/mok/it/app/mokapp/fragments/PhoneBookFragment.kt
+++ MOKApp/app/src/main/java/mok/it/app/mokapp/fragments/PhoneBookFragment.kt
@@ -4,18 +4,17 @@ import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.telephony.TelephonyManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
-import android.widget.TextView
 import androidx.core.content.ContextCompat
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.recyclerview.widget.RecyclerView
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter
 import com.firebase.ui.firestore.FirestoreRecyclerOptions
 import com.squareup.picasso.Picasso
-import kotlinx.android.synthetic.main.phone_contact_item.*
 import kotlinx.android.synthetic.main.phone_contact_item.view.*
 import mok.it.app.mokapp.R
 import mok.it.app.mokapp.baseclasses.BaseFireFragment
@@ -60,6 +59,11 @@ class PhoneBookFragment : BaseFireFragment() {
             WrapContentLinearLayoutManager(this.context)
     }
 
+    private fun isTelephonyEnabled(): Boolean {
+        val telephonyManager = getSystemService(this.requireContext(), TelephonyManager::class.java)
+        return (telephonyManager != null) && (telephonyManager.simState == TelephonyManager.SIM_STATE_READY)
+    }
+
     private fun initializeAdapter() {
         val options: FirestoreRecyclerOptions<User?> = FirestoreRecyclerOptions.Builder<User>()
             .setQuery(firestore.collection(userCollectionPath).orderBy("name"), User::class.java)
@@ -80,9 +84,29 @@ class PhoneBookFragment : BaseFireFragment() {
                     //TODO change to phone number
 
                     holder.itemView.call_button.setOnClickListener {
-                        val intent = Intent(Intent.ACTION_DIAL)
-                        intent.data = Uri.parse("tel:0123456789")
-                        this.context?.let { it2 -> ContextCompat.startActivity(it2, intent, null) }
+                        // if the device is capable of making phone calls, the button opens the dialer
+                        if (isTelephonyEnabled()) {
+                            val intent = Intent(Intent.ACTION_DIAL)
+                            //TODO change to their real number
+                            intent.data = Uri.parse("tel:0123456789")
+                            this.context?.let { it2 ->
+                                ContextCompat.startActivity(
+                                    it2,
+                                    intent,
+                                    null
+                                )
+                            }
+                        } else // ...if not, it copies the number to the clipboard
+                        {
+                            //TODO change to their real number
+                            val clipboard =
+                                this.context?.getSystemService(Context.CLIPBOARD_SERVICE) as android.content.ClipboardManager
+                            val clip = android.content.ClipData.newPlainText(
+                                "label",
+                                "0123456789"
+                            )
+                            clipboard.setPrimaryClip(clip)
+                        }
                     }
 
                     /* holder.itemView.sms_button.setOnClickListener {

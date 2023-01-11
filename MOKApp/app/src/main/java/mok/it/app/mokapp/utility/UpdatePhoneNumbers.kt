1f1c49227ce9366d970f90e562d103e894d5ce5c
Oliver Raisz <raiszoliver@gmail.com>
2023-01-11 09:46:42 +0100

    solved name match issue

--- MOKApp/app/src/main/java/mok/it/app/mokapp/utility/UpdatePhoneNumbers.kt
+++ MOKApp/app/src/main/java/mok/it/app/mokapp/utility/UpdatePhoneNumbers.kt
@@ -9,6 +9,7 @@ import java.text.Normalizer
 
 
 private var result = listOf<Person>()
+
 private class Person(val Név: String, val Telefonszám: String)
 
 private fun CharSequence.unaccent(): String {
@@ -21,19 +22,35 @@ private fun findPhoneNumber(
     keresztnev: String,
     masodikkeresztnev: String = ""
 ): String {
-    result.forEach() {
+    var count = 0
+    var phoneNumber = ""
+    result.forEach {
         val name = it.Név.unaccent()
         if (name.contains(vezeteknev) && (name.contains(keresztnev) || (masodikkeresztnev != "" && name.contains(
                 masodikkeresztnev
             )))
         )
-            return it.Telefonszám
+            if (count >= 1) {
+                Log.d(
+                    "UpdatePhoneNumbers",
+                    "Too many results, there are (at least partly) matching names: $vezeteknev $keresztnev $masodikkeresztnev"
+                )
+                return ""
+            } else {
+                count++
+                phoneNumber = it.Telefonszám
+            }
     }
-    Log.d(ContentValues.TAG, "findPhoneNumber: no phone number found for $vezeteknev $keresztnev")
-    return ""
+    return if (count == 0) {
+        Log.d(
+            ContentValues.TAG,
+            "findPhoneNumber: no phone number found for $vezeteknev $keresztnev"
+        )
+        ""
+    } else phoneNumber
 }
 
-fun updatePhoneNumbers(activity: Activity){
+fun updatePhoneNumbers(activity: Activity) {
     //phonenumbers.json file has to be in assets folder
     result = Klaxon().parseArray(activity.assets.open("phonenumbers.json"))!!
 
@@ -57,15 +74,20 @@ fun updatePhoneNumbers(activity: Activity){
                     vezeteknev = nev[2].unaccent()
                     phoneNumber = findPhoneNumber(vezeteknev, keresztnev, masodikkeresztnev)
                 } else {
-                    Log.d(ContentValues.TAG, "name length is not appropriate, skipping (name: $nev)")
+                    Log.d(
+                        ContentValues.TAG,
+                        "name length is not appropriate, skipping (name: $nev)"
+                    )
                     continue
                 }
                 //if the phoneNumber field is not already phoneNumber, update it
-                //Note that this doesn't work appropriately when there are people with the same name
-                // (not including middle name), e.g. "John Doe" and "John Jack Doe"
+                //if there are multiple people with the same name, the phone number will be empty
                 if (user["phoneNumber"] != phoneNumber) {
                     user.reference.update("phoneNumber", phoneNumber)
-                    Log.d(ContentValues.TAG, "updated the phone number of ${user["name"]}: $phoneNumber")
+                    Log.d(
+                        ContentValues.TAG,
+                        "updated the phone number of ${user["name"]}: $phoneNumber"
+                    )
                 }
             }
         } else {

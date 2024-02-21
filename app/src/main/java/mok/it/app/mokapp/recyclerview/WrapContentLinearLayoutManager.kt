bd072574d09f66266389776b3026f9a1810effc3
Oliver Raisz <raiszoliver@gmail.com>
2024-02-21 14:36:19 +0100

    restructured the file structure
    now it resembles a conventional structure (i.e. I removed a useless MOKApp folder inside the repository)

---
+++ app/src/main/java/mok/it/app/mokapp/recyclerview/WrapContentLinearLayoutManager.kt
@@ -0,0 +1,17 @@
+package mok.it.app.mokapp.recyclerview
+
+import android.content.Context
+import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.Recycler
+
+class WrapContentLinearLayoutManager(context: Context?) : LinearLayoutManager(context) {
+    override fun onLayoutChildren(recycler: Recycler, state: RecyclerView.State) {
+        try {
+            super.onLayoutChildren(recycler, state)
+        } catch (e: IndexOutOfBoundsException) {
+            Log.e("TAG", "meet a IOOBE in RecyclerView")
+        }
+    }
+}

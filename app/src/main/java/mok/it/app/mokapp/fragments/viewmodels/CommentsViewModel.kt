0116c73be61f2b7a7c0866c243ced7a913430659
Oliver Raisz <raiszoliver@gmail.com>
2024-04-16 12:08:39 +0200

    refactored comments fragment in compose

--- app/src/main/java/mok/it/app/mokapp/fragments/viewmodels/CommentsViewModel.kt
+++ app/src/main/java/mok/it/app/mokapp/fragments/viewmodels/CommentsViewModel.kt
@@ -2,9 +2,13 @@ package mok.it.app.mokapp.fragments.viewmodels
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import mok.it.app.mokapp.firebase.service.CommentService
 import mok.it.app.mokapp.firebase.service.UserService
+import mok.it.app.mokapp.model.Comment
 import mok.it.app.mokapp.model.User
 
 class CommentsViewModel : ViewModel() {
     fun getUserById(userId: String): LiveData<User> = UserService.getUser(userId)
+    fun getComments(projectId: String): LiveData<List<Comment>> =
+        CommentService.getComments(projectId)
 }

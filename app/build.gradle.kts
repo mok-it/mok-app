5e6fc2e7441804bd9a4b3fb287c474300dd51d2a
Oliver Raisz <raiszoliver@gmail.com>
2024-04-29 11:02:39 +0200

    updated version number to 5.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = (((Date().time / 1000) - 1451606400) / 10).toInt()
-        versionName = "5.0.0"
+        versionName = "5.0.1"
 
         testInstrumentationRunner =
             "androidx.test.runner.AndroidJUnitRunner"

f5eccbfcd626afc2f244927f23445a045dd407e8
Oliver Raisz <raiszoliver@gmail.com>
2024-04-29 11:02:00 +0200

    fixed uninitialized user exception when opening a deep link

--- app/src/main/java/mok/it/app/mokapp/feature/project_detail/DetailsFragment.kt
+++ app/src/main/java/mok/it/app/mokapp/feature/project_detail/DetailsFragment.kt
@@ -93,8 +93,16 @@ class DetailsFragment : Fragment() {
     ): View {
         setupTopMenu()
         return ComposeView(requireContext()).apply {
-            setContent {
-                DetailsScreen()
+            if (userModel.documentId.isEmpty()) {
+                refreshCurrentUserAndUserModel(requireContext()) {
+                    setContent {
+                        DetailsScreen()
+                    }
+                }
+            } else {
+                setContent {
+                    DetailsScreen()
+                }
             }
         }
     }
@@ -157,7 +165,6 @@ class DetailsFragment : Fragment() {
                         JoinAlertDialog(
                             onConfirm = {
                                 joinProject(project)
-                                refreshCurrentUserAndUserModel(requireContext())
                             },
                             onDismiss = {
                                 showDialog = DialogType.NONE
@@ -354,7 +361,7 @@ class DetailsFragment : Fragment() {
         },
         confirmButton = {
             Button(onClick = { onDismiss() }) {
-                Text("Close")
+                Text(stringResource(R.string.back))
             }
         }
     )
@@ -512,7 +519,6 @@ class DetailsFragment : Fragment() {
                     Toast.LENGTH_SHORT
                 ).show()
                 refreshCurrentUserAndUserModel(requireContext())
-                UserService.getMembersForProject(args.projectId)
             },
             {
                 Toast.makeText(
@@ -542,7 +548,6 @@ class DetailsFragment : Fragment() {
                 Toast.makeText(context, "Sikeresen lecsatlakoztál!", Toast.LENGTH_SHORT)
                     .show()
                 refreshCurrentUserAndUserModel(requireContext())
-                UserService.getMembersForProject(args.projectId)
             },
             {
                 Toast.makeText(

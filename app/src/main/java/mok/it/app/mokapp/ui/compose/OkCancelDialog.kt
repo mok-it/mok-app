453d6530fdbd149d9ba1e13a03248a601ecfeb6e
Oliver Raisz <raiszoliver@gmail.com>
2024-05-26 18:54:31 +0200

    added OkCancelDialog
    use this when you only need a confirm dialog

---
+++ app/src/main/java/mok/it/app/mokapp/ui/compose/OkCancelDialog.kt
@@ -0,0 +1,42 @@
+package mok.it.app.mokapp.ui.compose
+
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import mok.it.app.mokapp.R
+
+@Composable
+fun OkCancelDialog(
+    title: String? = null,
+    text: String,
+    positiveButtonText: String = "OK",
+    onConfirm: () -> Unit,
+    onDismiss: () -> Unit,
+) =
+    AlertDialog(
+        title = {
+            title?.let {
+                Text(it)
+            }
+        },
+        onDismissRequest = { onDismiss() },
+        confirmButton = {
+            Button(
+                onClick = {
+                    onConfirm()
+                    onDismiss()
+                }) {
+                Text(positiveButtonText)
+            }
+        },
+        dismissButton = {
+            OutlinedButton(onClick = { onDismiss() }) {
+                Text(stringResource(R.string.cancel))
+            }
+        },
+        text = {
+            Text(text)
+        })
